69b03c7754e6d7cfd36b375ca688abbc53dff3ca
Edson Zucchi <edsonluiz.zuchi@gmail.com>
2024-02-22 21:57:43 -0300

    first commit

---
+++ app/src/main/java/io/github/edsonzuchi/application/MainActivity.java
@@ -0,0 +1,72 @@
+package io.github.edsonzuchi.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int cont = 0;
+    TextView textView;
+
+    public void contar(View view){
+        cont++;
+        textView.setText(Integer.toString(cont));
+        Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        Log.d("callback", "onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        textView.setText(Integer.toString(0));
+        Log.d("callback", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("callback", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("callback", "onRestart");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("callback", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("callback", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("callback", "onDestroy");
+    }
+}
